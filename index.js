c48f54d8830f0780132ca91501112499bc19fcc3
qashka <78177504+qashka@users.noreply.github.com>
2021-11-04 16:57:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let myFunction;
+
+var h1number = document.getElementsByTagName("h1").length;
+console.log(h1number);
+
+var thing1number = document.getElementById("checkboxName").length;
+console.log(thing1number);
